e3e21adba6160542611d9a56db2343457d7c41dd
Udit Vasu <udit@postman.com>
2020-09-11 09:11:26 +0530

    Test: Handle Node v6 VM bug

--- test/unit/uvm-sanity.test.js
+++ test/unit/uvm-sanity.test.js
@@ -89,20 +89,48 @@ describe('uvm', function () {
             uvm.spawn({
                 bootCode: `
                     bridge.on('getProps', function () {
-                        bridge.dispatch('getProps', Object.getOwnPropertyNames(Function('return this')()));
+                        var globalContext = Function('return this')(),
+                            data = {};
+
+                        Object.getOwnPropertyNames(globalContext).forEach(function (prop) {
+                            if (prop.indexOf('__uvm') !== -1) {
+                                data[prop] = String(globalContext[prop]);
+                            }
+                        });
+
+                        bridge.dispatch('getProps', data);
+
+                        // Use this when we drop support for Node v6
+                        // bridge.dispatch('getProps', Object.getOwnPropertyNames(Function('return this')()));
                     });
                 `
             }, function (err, context) {
                 expect(err).to.be.null;
 
+                var nodeVersion = process && process.versions && parseInt(process.versions.node);
+
                 context.on('error', done);
                 context.on('getProps', function (data) {
-                    expect(data).to.be.an('array').that.is.not.empty;
+                    expect(data).to.be.an('object');
 
-                    for (var key of data) {
-                        expect(key).to.not.have.string('__uvm');
+                    // In Node.js v6 VM, `delete global` doesn't work so we make
+                    // sure it's set to undefined.
+                    if (nodeVersion === 6) {
+                        for (var prop in data) { // eslint-disable-line guard-for-in
+                            expect(data[prop]).to.equal('undefined');
+                        }
+                        return done();
                     }
+
+                    expect(data).to.be.empty;
                     done();
+
+                    // Use this when we drop support for Node v6
+                    // expect(data).to.be.an('array').that.is.not.empty;
+
+                    // for (var key of data) {
+                    //     expect(key).to.not.have.string('__uvm');
+                    // }
                 });
 
                 context.dispatch('getProps');
