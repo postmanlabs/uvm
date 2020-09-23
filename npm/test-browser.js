220411dba82688feb32f96ec58749d664208d8c0
Udit Vasu <udit@pm.me>
2020-09-23 22:20:02 +0530

    Remove puppeteer dependency

--- npm/test-browser.js
+++ npm/test-browser.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
+/* eslint-env node, es6 */
 // ---------------------------------------------------------------------------------------------------------------------
 // This script is intended to execute all unit tests in the Chrome Browser.
 // ---------------------------------------------------------------------------------------------------------------------
-/* eslint-env node, es6 */
-
-require('shelljs/global');
 
 var chalk = require('chalk'),
     path = require('path'),
@@ -12,9 +10,14 @@ var chalk = require('chalk'),
     KARMA_CONFIG_PATH = path.join(__dirname, '..', 'test', 'karma.conf');
 
 module.exports = function (exit) {
-    console.log(chalk.yellow.bold('Running unit tests within browser...'));
+    if (process.env.TRAVIS_OS_NAME === 'windows') { // eslint-disable-line no-process-env
+        return console.info(chalk.yellow.bold('Skipping browser tests on windows...'));
+    }
+
+    console.info(chalk.yellow.bold('Running unit tests within browser...'));
 
     var KarmaServer = require('karma').Server;
+
     (new KarmaServer({ // eslint-disable no-new
         cmd: 'start',
         configFile: KARMA_CONFIG_PATH
@@ -22,4 +25,4 @@ module.exports = function (exit) {
 };
 
 // ensure we run this script exports if this is a direct stdin.tty run
-!module.parent && module.exports(exit);
+!module.parent && module.exports(process.exit);
