4b2f5f10201fe785d2d11be4d945c874fe77f0b8
Udit Vasu <udit@pm.me>
2020-10-01 16:18:13 +0530

    Update npm scripts

--- npm/test-lint.js
+++ npm/test-lint.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-env node, es6 */
 // ---------------------------------------------------------------------------------------------------------------------
 // This script is intended to contain all actions pertaining to code style checking, linting and normalization.
 // ---------------------------------------------------------------------------------------------------------------------
