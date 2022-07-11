ff90a1dda2ae991e986b738a10a60e7799cfb9fd
Udit Vasu <udit@pm.me>
2022-07-11 12:18:34 +0530

    Updated Flatted dependency

--- test/system/locked-flatted.test.js
+++ test/system/locked-flatted.test.js
@@ -2,11 +2,12 @@ const expect = require('chai').expect;
 
 // There is a string variant of the library in bridge-client.js
 describe('flatted dependency', function () {
-    // To update flatted, the package needs to be updated and then post installing flatted, one needs to
-    // manually copy node_modules/flatted/min.js and replace it in where the previous
-    // circular-json code existed within lib/bridge-client.js. Finally, we should replace all backslash "\"
+    // To update flatted, the package needs to be updated and then post
+    // installing flatted, one needs to manually copy 'node_modules/flatted/min.js'
+    // and replace it in where the previous flatted code existed within
+    // 'lib/bridge-client.js'. Finally, we should replace all backslash "\"
     // characters with double backslash "\\". (if any)
     it('should be version locked, unless modified intentionally', function () {
-        expect(require('../../package.json').dependencies.flatted).to.equal('3.1.1');
+        expect(require('../../package.json').dependencies.flatted).to.equal('3.2.6');
     });
 });
