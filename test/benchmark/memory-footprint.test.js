85d1024a982aff3b0810d9025de19fe5fc8f8bdf
Appurva Murawat <appurva21@proton.me>
2024-08-26 23:25:24 +0530

    Reduce memory footprint

---
+++ test/benchmark/memory-footprint.test.js
@@ -0,0 +1,10 @@
+
+// TODO: Implement this test
+// v8.getHeapStatistics() can be used to measure memory
+// footprint but the results from it are not consistent with
+// values from heap snapshots created manually in Chrome DevTools.
+describe.skip('memory footprint', function () {
+    it('should not increase memory footprint significantly', function (done) {
+        done();
+    });
+});
